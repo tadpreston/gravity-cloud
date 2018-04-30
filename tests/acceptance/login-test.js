4ad9817880948ebaaaa92bd40cd182f22f1002c9
Arden Zhan <arden@assetpanda.com>
2018-04-30 16:53:59 -0500

    acceptance tests for links, need to create routes

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | login', function(hooks) {
@@ -10,31 +10,33 @@ module('Acceptance | login', function(hooks) {
     assert.equal(currentURL(), '/login');
   });
 
-  test('should redirect to app if user already signed in', async (assert) => {
+  // test('should redirect to app if user already signed in', async (assert) => {
+  // });
 
-  });
+  // test('should redirect to app if login credentials valid', async (assert) => {
+  // });
 
-  test('should redirect to app if login credentials valid', async (assert) => {
+  // test('should stay on login page if login credentials invalid', async (assert) => {
+  // });
 
-  });
+  // test('should show error message if login credentials invalid', async (assert) => {
+  // });
 
-  test('should stay on login page if login credentials invalid', async (assert) => {
-
-  });
-
-  test('should show error message if login credentials invalid', async (assert) => {
-
-  });
-
-  test('should link to sign up form', async (assert) => {
-
-  });
-
-  test('should link to forget password form', async (assert) => {
-
-  });
-
-  test('should link to terms of use form', async (assert) => {
-
-  });
+  // test('should link to sign up form', async (assert) => {
+  //   await visit('/login');
+  //   await click('.register');
+  //   assert.equal(currentURL(), 'https://www.assetpanda.com/register-free/', 'should navigate to sign up form');
+  // });
+  
+  // test('should link to terms of use form', async (assert) => {
+  //   await visit('/login');
+  //   await click('.terms');
+  //   assert.equal(currentURL(), 'https://www.assetpanda.com/terms-of-use/', 'should navigate to terms of use form');
+  // });
+  
+  // test('should link to forgot password form', async (assert) => {
+  //   await visit('/login');
+  //   await click('.forgot');
+  //   assert.equal(currentURL(), 'https://login.assetpanda.com/users/password/new', 'should navigate to forgot password form');
+  // });
 });
